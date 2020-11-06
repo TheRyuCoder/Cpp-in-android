dfc394b9b4da6395b4e6e1d00135ac9259b65f65
TheRyuCoder <73657385+TheRyuCoder@users.noreply.github.com>
2020-11-06 22:56:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ echo -e "Updating and Upgarding"
 clr 37
 pkg update && pkg upgrade
 clr 31
-echo -e"Installing C/C++ Complier"
+echo -e "Installing C/C++ Complier"
 clr 37
 pkg install clang
 clr 31
-echo -e"Setting up Env "
+echo -e "Setting up Env "
 clr 37
 cp .bash_aliases -r ~
 cp .bashrc -r ~
 source ~/.bashrc
 clr 32
-echo -e"Install Complted"
+echo -e "Install Complted"
 clr 37
 
 
