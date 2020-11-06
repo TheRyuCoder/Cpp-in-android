1422b4d1a44865835b226537e8d5ede72b4373d5
TheRyuCoder <73657385+TheRyuCoder@users.noreply.github.com>
2020-11-06 22:36:24 +0530

    Update and rename README.md to setup.sh

--- README.md
+++
@@ -1,1 +0,0 @@
-# Cpp-in-android
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash  
+clr(){
+  echo -e "\e["$1"m" $2
+  }
+clr 31
+echo -e "Updating and Upgarding"
+clr 37
+pkg update && pkg upgrade
+clr 31
+echo -e"Installing C/C++ Complier"
+clr 37
+pkg install clang
+clr 31
+echo -e"Setting up Env "
+clr 37
+cp .bash_aliases -r ~
+cp .bashrc -r ~
+source ~/.bashrc
+clr 32
+echo -e"Install Complted"
+clr 37
+c
+
+
